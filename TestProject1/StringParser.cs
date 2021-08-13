03f0c70cf892d3cd8f8dd876b0729dcb6b43007b
Allen <g01.webapp@gmail.com>
2021-08-13 11:03:58 +0800

    內容有 at 開頭表示要 cc 的對象, 可能是零個,一個或多個

--- TestProject1/StringParser.cs
+++ TestProject1/StringParser.cs
@@ -1,4 +1,6 @@
 ﻿using System.Dynamic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace TestProject1
 {
@@ -7,6 +9,15 @@ namespace TestProject1
         public ParseResult Parse(string context)
         {
             return new ParseResult { Message = context };
+        }
+
+        public string[] GetCCs(string context)
+        {
+            var pattern = @"(^@\w[\S]*)|([^@]@\w[\S]*)"; // 開頭不是at且緊接著一段文字 or 字串中, at的前面不是 at且緊接著一段文字
+            Regex rx = new Regex(pattern);
+            MatchCollection matches = rx.Matches(context);
+
+            return matches.Cast<Match>().Select(x => x.Value.Trim().Substring(1)).ToArray();
         }
     }
 
--- TestProject1/Tests.cs
+++ TestProject1/Tests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 /*
-[working on] 解析傳回訊息內容
+[V] 解析傳回訊息內容
+[working on] 內容有 at 開頭表示要 cc 的對象, 可能是零個,一個或多個
  */
 namespace TestProject1
 {
@@ -9,7 +11,7 @@ namespace TestProject1
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void Parse_WhenCalled()
         {
             string context = "abc";
             var sut = new StringParser();
@@ -18,5 +20,40 @@ namespace TestProject1
             
             Assert.AreEqual(context, actual.Message);
         }
+        
+        [TestCase("@abc", "abc")]
+        [TestCase("@abc def", "abc")]
+        [TestCase("123 @abc def", "abc")]
+        public void GetCCs_一個符合_傳回陣列(string context, string expected)
+        {
+            var sut = new StringParser();
+            
+            var actual = sut.GetCCs(context);
+            
+            Assert.AreEqual(expected, actual[0]);
+        }
+        
+        [TestCase("")]
+        [TestCase("abc")]
+        public void GetCCs_沒有符合_傳回空陣列(string context)
+        {
+            var sut = new StringParser();
+            
+            var actual = sut.GetCCs(context);
+            
+            Assert.IsTrue(actual.Length==0);
+        }
+        
+        [TestCase("@abc @def 123", "abc;def")]
+        public void GetCCs_多個符合_傳回陣列(string context, string expected)
+        {
+            var sut = new StringParser();
+            
+            var result = sut.GetCCs(context);
+            string actual = result.Aggregate((acc, next) => acc + ";" + next);
+            
+            Assert.AreEqual(expected, actual);
+        }
+        
     }
 }
