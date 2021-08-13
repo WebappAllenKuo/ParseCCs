e88427d23d0ef9bbe82593c3c72b273efda0a31d
Allen <g01.webapp@gmail.com>
2021-08-13 10:51:33 +0800

    解析傳回訊息內容

---
+++ TestProject1/StringParser.cs
@@ -0,0 +1,18 @@
+﻿using System.Dynamic;
+
+namespace TestProject1
+{
+    public class StringParser
+    {
+        public ParseResult Parse(string context)
+        {
+            return new ParseResult { Message = context };
+        }
+    }
+
+    public class ParseResult
+    {
+        public string[] CCs { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ TestProject1/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+/*
+[working on] 解析傳回訊息內容
+ */
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            string context = "abc";
+            var sut = new StringParser();
+            
+            var actual = sut.Parse(context);
+            
+            Assert.AreEqual(context, actual.Message);
+        }
+    }
+}
